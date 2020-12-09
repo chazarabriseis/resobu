39082a387ad0c5ac82f1c4827609221c1defd10e
Julia Baldauf <julia.s.baldauf@gmail.com>
2020-12-09 10:53:41 +0100

    added temporarily image from slideshow

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -15,9 +15,11 @@ class AboutUs extends React.Component {
       <div>
         <div className="topSection">
           <div className="heading1"> The Team</div>
-          <div className="heading2">More information coming soon</div>
+          <div className="heading2">We believe that communication is the key to healthy organization culture and networks which in turn is the key to innovation and success.</div>
         </div>
-        <div className="contactContent"></div>
+        <div className="contactContent">
+          <img alt="howIcon" className= "team-image" src="ReSoBuTeam.png" />
+        </div>
       </div>
     )
   }
